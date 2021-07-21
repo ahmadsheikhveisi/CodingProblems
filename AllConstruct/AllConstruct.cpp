bcae5a25f4a36ba286d13232898965cf1e1fc5ea
Ahmad Sheikhveisi <ahmad.sheikhveisi87@gmail.com>
2021-07-21 23:03:01 +0200

    adding tabluation and string view to all construct

--- AllConstruct/AllConstruct.cpp
+++ AllConstruct/AllConstruct.cpp
@@ -8,24 +8,24 @@
 #include <vector>
 #include <unordered_map>
 #include <string>
+#include <algorithm>
 
 // time complexity O(size)
 // space complexity
-std::vector<std::vector<std::string>>
-AllConstruct_(const std::string& targetStr,
-    const std::vector<std::string> wordBank,
-    std::unordered_map<std::string,
-        std::vector<std::vector<std::string>>>* hashTable) {
+std::vector<std::vector<std::string_view>>
+AllConstruct_(std::string_view targetStr,
+    const std::vector<std::string_view>& wordBank,
+    std::unordered_map<std::string_view,
+        std::vector<std::vector<std::string_view>>>* hashTable) {
     if (hashTable->find(targetStr) != hashTable->end()) {
         return hashTable->at(targetStr);
     }
-    std::vector<std::vector<std::string>> res;
-    for (const auto& word : wordBank) {
-        std::vector<std::string> word_res;
+    std::vector<std::vector<std::string_view>> res;
+    for (auto word : wordBank) {
+        std::vector<std::string_view> word_res;
         auto pos = targetStr.find(word);
         if (pos != std::string::npos && pos == 0) {
-            std::string temp{targetStr};
-            temp.erase(pos, word.size());
+            auto temp = targetStr.substr(pos + word.size());
             if (temp.empty()) {
                 word_res.push_back(word);
                 res.push_back(word_res);
@@ -35,7 +35,7 @@ AllConstruct_(const std::string& targetStr,
                     word_res.push_back(word);
                 }
                 for (const auto& cnt_res : temp_res) {
-                    std::vector<std::string> temp_word_res{word_res};
+                    std::vector<std::string_view> temp_word_res{word_res};
                     temp_word_res.insert(temp_word_res.end(),
                         cnt_res.begin(), cnt_res.end());
                     res.push_back(temp_word_res);
@@ -47,17 +47,49 @@ AllConstruct_(const std::string& targetStr,
     return res;
 }
 
-std::vector<std::vector<std::string>>
-AllConstruct(const std::string& targetStr,
-    const std::vector<std::string> wordBank) {
-    std::unordered_map<std::string,
-        std::vector<std::vector<std::string>>> hashTable;
-    return AllConstruct_(targetStr, wordBank, &hashTable);
+std::vector<std::vector<std::string_view>>
+AllConstruct_table(std::string_view targetStr,
+    const std::vector<std::string_view>& wordBank) {
+    std::vector<std::vector<std::vector<std::string_view>>>
+        res(targetStr.size() + 1);
+    for (auto word : wordBank) {
+        if (targetStr.substr(0, word.size()) == word) {
+            res[word.size()].push_back(std::vector<std::string_view>{word});
+        }
+    }
+    for (size_t cnt = 1; cnt < res.size(); ++cnt) {
+        if (res[cnt].size() != 0) {
+            for (auto word : wordBank) {
+                if (targetStr.substr(cnt, word.size()) == word) {
+                    auto temp_res = res[cnt];
+                    std::for_each(begin(temp_res), end(temp_res),
+                        [&](std::vector<std::string_view>& pvec){
+                            pvec.push_back(word);
+                        });
+                    res[cnt + word.size()].insert(
+                        res[cnt + word.size()].end(),
+                        std::make_move_iterator(temp_res.begin()),
+                        std::make_move_iterator(temp_res.end()));
+                }
+            }
+        }
+    }
+
+    return res.back();
 }
 
-void printres(const std::vector<std::vector<std::string>> res) {
+std::vector<std::vector<std::string_view>>
+AllConstruct(std::string_view targetStr,
+    const std::vector<std::string_view>& wordBank) {
+    // std::unordered_map<std::string_view,
+    //    std::vector<std::vector<std::string_view>>> hashTable;
+    // return AllConstruct_(targetStr, wordBank, &hashTable);
+    return AllConstruct_table(targetStr, wordBank);
+}
+
+void printres(const std::vector<std::vector<std::string_view>>& res) {
     for (const auto& vec : res) {
-        for (const auto& word : vec) {
+        for (const auto word : vec) {
             std::cout << word << ' ';
         }
         std::cout << '\n';
@@ -75,7 +107,7 @@ int main() {
                   "def", "abcd", "ef", "c"}));
     printres(AllConstruct("", {"ab", "abc", "cd",
                   "def", "abcd", "ef", "c"}));
-    printres(AllConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+    printres(AllConstruct("eeeeeeeeeeeeeeeeeeeeeef",
                 {"e", "ee", "eee", "eeee", "eeeeee",
                 "eeeeeee", "eeeeeeee", "eeeeeeeee"}));
     return 0;

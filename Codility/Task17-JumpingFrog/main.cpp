7ee63fe832ea469ffef6ea0320d08cc90b0b8c1b
Ahmad Sheikhveisi <ahmad.sheikhveisi87@gmail.com>
2021-12-18 16:44:47 +0100

    adding cmake to whole project

---
+++ Codility/Task17-JumpingFrog/main.cpp
@@ -0,0 +1,15 @@
+/* COPYRIGHT
+ *
+ * A small frog wants to get from position 0 to k (1 <= k <= 10 000). The frog can
+ * jump over any one of n fixed distances s_0 , s_1 , . . . , s_(n−1) (1 <= s_i <= k). The goal is to count the
+ * number of different ways in which the frog can jump to position k. To avoid overflow, it is
+ * sufficient to return the result modulo q, where q is a given number.
+ * We assume that two patterns of jumps are different if, in one pattern, the frog visits
+ * a position which is not visited in the other pattern.
+ * 
+ * 
+ */
+
+int main() {
+    return 0;
+}

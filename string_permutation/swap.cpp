4cb36646e4b38026b6b50069676b092d975518c8
Ahmad Sheikhveisi <ahmad.sheikhveisi87@gmail.com>
2023-04-01 22:48:25 +0200

    string permutation swap unique

--- string_permutation/swap.cpp
+++ string_permutation/swap.cpp
@@ -6,21 +6,37 @@
 #include <string_view>
 #include <vector>
 #include <iostream>
+#include <unordered_set>
 
-void permutation(std::string& str, size_t const left, size_t const right) {
+void permutation(std::string& str, size_t const left, size_t const right,
+                 std::unordered_set<std::string>& mset) {
     if (left == right) {
-        std::cout << str << '\n';
+        std::cout << str;
+        auto itr = mset.find(str);
+        if (itr != mset.end()) {
+            std::cout << " !";
+        } else {
+            mset.insert(str);
+        }
+        std::cout << '\n';
     } else {
         for (size_t cnt = left; cnt <= right; ++cnt) {
+            if (cnt > left && str[left] == str[cnt]) {
+                continue;
+            }
+            if (cnt > left && str[cnt - 1] == str[cnt]) {
+                continue;
+            }
             std::swap(str[left], str[cnt]);
-            permutation(str, left + 1, right);
+            permutation(str, left + 1, right, mset);
             std::swap(str[left], str[cnt]);
         }
     }
 }
 
 int main() {
-    std::string str{"123"};
-    permutation(str, 0 , str.size() - 1);
+    std::string str{"ABBB"};
+    std::unordered_set<std::string> mset;
+    permutation(str, 0 , str.size() - 1, mset);
     return 0;
 }

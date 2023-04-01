4d2ff849fbddb8804b5388bd670bd18eb7fd5734
Ahmad Sheikhveisi <ahmad.sheikhveisi87@gmail.com>
2023-04-01 19:38:10 +0200

    string unique permutation unordered set

--- string_permutation/main.cpp
+++ string_permutation/main.cpp
@@ -6,10 +6,15 @@
 #include <string_view>
 #include <vector>
 #include <iostream>
+#include <unordered_set>
 
-void permutation(std::string_view prefix, std::string_view str) {
+void permutation(std::string prefix, std::string str,
+                std::unordered_set<std::string> *mset) {
     if (str.size() == 0) {
-        std::cout << "out " << prefix << '\n';
+        if (mset->find(prefix) == mset->end()) {
+            std::cout << "out " << prefix << '\n';
+            mset->insert(prefix);
+        }
     } else {
         // std::cout << '\n';
         // std::cout << "prefix " << prefix << " str " << str << '\n';
@@ -21,18 +26,19 @@ void permutation(std::string_view prefix, std::string_view str) {
             auto pre2 = std::string{str[i]};
             auto pre = std::string(prefix) + pre2;
             // std::cout << i << " pre " << pre << " rem " << rem << '\n';
-            permutation(pre, rem);
+            permutation(pre, rem, mset);
          }
         // std::cout << "return " << prefix << " str " << str << '\n';
     }
 }
 
-void permutation(std::string_view str) {
-    permutation("", str);
+void permutation(std::string str) {
+    std::unordered_set<std::string> mset;
+    permutation("", str, &mset);
 }
 
 
 int main(void) {
-    permutation("1234");
+    permutation("1114");
     return 0;
 }

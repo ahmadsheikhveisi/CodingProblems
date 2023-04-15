7ad74d5a64c09650f4c150e14e61b65cb7b49973
Ahmad Sheikhveisi <ahmad.sheikhveisi87@gmail.com>
2023-04-15 23:27:23 +0200

    add move to stack

--- stack_impl/main.cpp
+++ stack_impl/main.cpp
@@ -1,15 +1,76 @@
 /* COPYRIGHT
 */
 #include <iostream>
+#include <vector>
 
 #include "stack.hpp"
 
+class mtype {
+ public:
+    std::string name;
+    mtype() {
+        name = "default ctor";
+        std::cout << "Constructed: " << name << '\n';
+    }
+    explicit mtype(std::string const& str) {
+        name = str;
+        std::cout << "Constructed: " << name << '\n';
+    }
+    ~mtype() {
+        std::cout << name << " destructed\n";
+    }
+    mtype(mtype&& other) {
+        name += " " + std::move(other.name) + " Moved";
+        std::cout << "Moved constructed: " << name << '\n';
+    }
+    mtype(mtype const & other) {
+        name += " " + other.name + " Copied";
+        std::cout << "Copied constructed: " << name << '\n';
+    }
+    mtype& operator=(mtype const & other) {
+        name += " " + other.name + " Assign copied";
+        std::cout << "Assgined copied: " << name << '\n';
+        return *this;
+    }
+    mtype& operator=(mtype&& other) {
+        name += " " + std::move(other.name) + " Moved assign";
+        std::cout << "Move assigned: " << name << '\n';
+        return *this;
+    }
+    const std::string & print() const {
+        return name;
+    }
+};
+
 int main() {
-    stack_impl::stack<int> msck;
-    msck.push(0);
+    stack_impl::stack<mtype> msck;
+    {
+        mtype data1{"Temp"};
+        std::cout << "Created\n";
+        msck.push(std::move(data1));
+        std::cout << "Pushed\n";
+    }
+    std::cout << "top is: " << msck.top().name << '\n';
+
+    /*msck.push(0);
     msck.push(1);
     std::cout << msck.top() << '\n';
+    std::cout << msck.size() << '\n';
     msck.pop();
     std::cout << msck.top() << '\n';
+    std::cout << msck.size() << '\n';
+    msck.pop();
+    std::cout << msck.size() << '\n';
+    msck.pop();
+    std::cout << msck.size() << '\n';*/
+
+    std::vector<mtype> vec;
+    {
+        mtype const data1{"Temp"};
+        std::cout << "Created\n";
+        vec.emplace_back(std::move(data1));
+        std::cout << "Pushed\n";
+    }
+    std::cout << vec.back().name << '\n';
     return 0;
 }
--- stack_impl/stack.hpp
+++ stack_impl/stack.hpp
@@ -6,7 +6,9 @@
 
 #include <signal.h>
 
+#include <iostream>
 #include <memory>
+#include <utility>
 
 namespace stack_impl {
 template<typename T>
@@ -16,23 +18,46 @@ class stack {
      public:
         T data;
         std::shared_ptr<Node> next{nullptr};
-        explicit Node(T t_data): data(t_data) {}
+        explicit Node(T&& t_data): data(std::move(t_data)) {
+            std::cout << "Creating data from: " << t_data.print();
+            // data = std::move(t_data);
+            std::cout << " To:" << data.print() << '\n';
+        }
+        explicit Node(const T& t_data): data(std::move(t_data)) {
+            std::cout << "Copying data from: " << t_data.print();
+            // data = std::move(t_data);
+            std::cout << " To:" << data.print() << '\n';
+        }
     };
     size_t size_;
     std::shared_ptr<Node> top_;
 
- public:
-    stack() : size_(0), top_{nullptr} {}
-    void push(T val) {
-        auto ptr = std::make_shared<Node>(val);
+    void set_top(std::shared_ptr<Node> ptr) {
         if (top_ == nullptr) {
             top_ = ptr;
         } else {
             ptr->next = top_;
             top_ = ptr;
         }
+    }
+
+ public:
+    stack() : size_(0), top_{nullptr} {}
+    void push(T&& val) {
+        std::cout << "Forwarding: " << val.print() << '\n';
+        auto ptr = std::make_shared<Node>(std::forward<T>(val));
+        std::cout << "Setting top: " << ptr->data.print() << '\n';
+        set_top(ptr);
         ++size_;
     }
+    void push(const T& val) {
+        std::cout << "Copying: " << val.print() << '\n';
+        auto ptr = std::make_shared<Node>(val);
+        std::cout << "Setting top: " << ptr->data.print() << '\n';
+        set_top(ptr);
+        ++size_;
+    }
+
     void pop() {
         if (top_ != nullptr) {
             top_ = top_->next;

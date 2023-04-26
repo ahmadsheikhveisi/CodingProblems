302acbf93290dab8d2d7416fc36f8272c469984b
ahmad.sheikhveisi <ahmad.sheikhveisi87@gmail.com>
2023-04-26 19:59:13 +0200

    Check all unique chars

---
+++ Cracking/1-CheckAllUniqueLetters/main.cpp
@@ -0,0 +1,89 @@
+/* COPYRIGHT
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <string_view>
+#include <type_traits>
+#include <vector>
+#include <unordered_set>
+
+bool CheckAllUniqueCharBitVector(std::string_view str) {
+    if (str.size() == 0) {
+        return true;
+    }
+    size_t hash_size{
+        static_cast<size_t>(0x1) << (sizeof(str[0]) * 8)};
+    if (str.size() > hash_size) {
+        return false;
+    }
+    std::vector<bool> hash(hash_size, false);
+    for (auto chr : str) {
+        if (hash[static_cast<size_t>(chr)]) {
+            return false;
+        } else {
+            hash[static_cast<size_t>(chr)] = true;
+        }
+    }
+    return true;
+}
+
+bool CheckAllUniqueCharN2NoSpace(std::string_view str) {
+    if (str.size() == 0) {
+        return true;
+    }
+    if (str.size() >
+        (static_cast<size_t>(0x1) << (sizeof(str[0]) * 8))) {
+        return false;
+    }
+    for (size_t cnt1{0}; cnt1 < str.size(); ++cnt1) {
+        for (size_t cnt2{0}; cnt2 < str.size(); ++cnt2) {
+            if (cnt1 != cnt2 && str[cnt1] == str[cnt2]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool CheckAllUniqueLettersSortNLogN(std::string str) {
+    std::sort(begin(str), end(str));
+    for (size_t cnt{1}; cnt < str.size(); ++cnt) {
+        if (str[cnt] == str[cnt - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template<typename T>
+bool CheckAllUniqueCharHash(T str) {
+    if (str.size() == 0) {
+        return true;
+    }
+    if (str.size() >
+        (static_cast<size_t>(0x1) << (sizeof(str[0]) * 8))) {
+        return false;
+    }
+    std::unordered_set<std::decay_t<decltype(str[0])>> hash;
+    for (auto chr : str) {
+        if (hash.find(chr) != hash.end()) {
+            return false;
+        } else {
+            hash.insert(chr);
+        }
+    }
+    return true;
+}
+
+int main() {
+    std::cout << CheckAllUniqueCharBitVector("asb4B/") << '\n';
+    std::cout << CheckAllUniqueCharN2NoSpace("asb4B/") << '\n';
+    std::cout << CheckAllUniqueLettersSortNLogN("asb4B/") << '\n';
+    std::cout << CheckAllUniqueLettersSortNLogN("asMnkaKb4B/") << '\n';
+    std::cout << CheckAllUniqueCharHash(std::string_view("a/sb4B/")) << '\n';
+    std::cout << CheckAllUniqueCharHash(std::wstring(L"asb4B/")) << '\n';
+    std::cout << CheckAllUniqueCharHash(
+        std::wstring(L"به استند۲")) << '\n';
+    return 0;
+}
--- FindCommonSorted/main.cpp
+++ FindCommonSorted/main.cpp
@@ -2,6 +2,8 @@
  * Given two sorted arrays, find all common members
 */
 
+#include <algorithm>
+#include <array>
 #include <iostream>
 #include <vector>
 
@@ -21,8 +23,8 @@ std::vector<int> solution(std::vector<int> const & a,
     return res;
 }
 
-void printvec(std::vector<int> vec) {
-    for (auto mem : vec) {
+void printvec(const std::vector<int>& vec) {
+    for (const auto mem : vec) {
         std::cout << mem << ' ';
     }
     std::cout << '\n';
@@ -30,8 +32,14 @@ void printvec(std::vector<int> vec) {
 
 int main() {
     printvec(solution({1, 3, 5, 8, 10}, {2, 5, 6, 7, 8}));
-    printvec(solution({1, 3, 5, 7, 9}, {2, 5, 6, 7, 8}));
+    printvec(solution({1, 5, 5, 7, 9}, {2, 5, 6, 7, 8}));
     printvec(solution({13, 27, 35, 40, 49, 55, 59},
         {17, 35, 39, 40, 55, 58, 60}));
+    auto vec = {3, 9, 1, 4, 2, 5, 9};  //  std::vector({3, 9, 1, 4, 2, 5, 9});
+    auto [min, max] = std::minmax_element(begin(vec), end(vec));
+    std::cout << std::distance(begin(vec), min) << ' ' <<
+        std::distance(begin(vec), max) << '\n';
+    //  std::swap(*min, *max);
+    printvec(vec);
     return 0;
 }

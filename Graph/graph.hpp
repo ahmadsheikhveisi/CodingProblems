9040fc6f55a1e227c748b913e5619d84dbf1ee85
Ahmad Sheikhveisi <ahmad.sheikhveisi@volvocars.com>
2023-09-21 17:29:45 +0200

    Add length to edge class

--- Graph/graph.hpp
+++ Graph/graph.hpp
@@ -17,8 +17,9 @@ class Graph {
  public: 
  class Edge {
     public:
-    Edge(int cost = 0) : cost_{cost} {}
+    Edge(int cost = 0, int length = 0) : cost_{cost}, length_{length} {}
     int cost_;
+    int length_;
  };
  class Vertex {
     public:
@@ -29,6 +30,11 @@ class Graph {
     // and if we use only int for cost of the edge, we need more than one edge with a cost
     // the only solution might be multimap, which still doesn't help in searching.
     std::vector<std::pair<std::shared_ptr<Vertex>, Edge>> adjacency_list_;
+
+    void AddVertex(std::shared_ptr<Vertex> to_vertex, Edge edge) {
+        auto new_edge = std::make_pair(to_vertex, edge);
+        adjacency_list_.emplace_back(new_edge);
+    }
  };
 
  std::unordered_map<std::string, std::shared_ptr<Vertex>> graph{};
@@ -48,20 +54,18 @@ class Graph {
     return true;
  }
 
- bool AddEdge(std::string const& from, std::string const& to, int cost) {
+ bool AddEdge(std::string const& from, std::string const& to, int cost = 0, int length = 0) {
     auto from_it = graph.find(from);
     auto to_it = graph.find(to);
     if ((from_it == end(graph)) || (to_it == end(graph))) {
         return false;
     }
-    auto new_edge = std::make_pair(to_it->second, Edge{cost});
-    from_it->second->adjacency_list_.emplace_back(new_edge);
-
+    from_it->second->AddVertex(to_it->second, Edge{cost, length});
     return true;
  }
 
  using Visit = std::function<bool(std::shared_ptr<Vertex>)>;
- 
+
  void BreadthFirstSearch(std::shared_ptr<Vertex> vertex, Visit visit) {
     std::queue<std::shared_ptr<Vertex>> mqueue;
     vertex->visited_ = true;

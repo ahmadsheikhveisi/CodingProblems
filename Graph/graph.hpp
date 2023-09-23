098c5c89dcbbdf6c2b8791c09962004f8b55a103
Ahmad Sheikhveisi <ahmad.sheikhveisi87@gmail.com>
2023-09-24 00:05:53 +0200

    Add graph bidirectional search

--- Graph/graph.hpp
+++ Graph/graph.hpp
@@ -10,6 +10,7 @@
 #include <queue>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <utility>
 #include <vector>
 
@@ -17,13 +18,14 @@ class Graph {
  public:
   class Edge {
    public:
-    Edge(int cost = 0, int length = 0) : cost_{cost}, length_{length} {}
+    explicit Edge(int cost = 0, int length = 0)
+        : cost_{cost}, length_{length} {}
     int cost_;
     int length_;
   };
   class Vertex {
    public:
-    Vertex(const std::string& name)
+    explicit Vertex(const std::string& name)
         : name_{name}, adjacency_list_{}, visited_{false} {}
     const std::string& name_;
     bool visited_;
@@ -39,36 +41,57 @@ class Graph {
     }
   };
 
-  std::unordered_map<std::string, std::shared_ptr<Vertex>> graph{};
+  explicit Graph(bool directed = true) : directed_{directed}, graph_{} {}
+
+  const bool directed_;
+  std::unordered_map<std::string, std::shared_ptr<Vertex>> graph_{};
+
+  std::shared_ptr<Vertex> FindVertedByName(std::string_view name) {
+    auto it = graph_.find(std::string(name));
+    if (it == end(graph_)) {
+      return nullptr;
+    }
+    return it->second;
+  }
 
   void Reset() {
-    for (auto& vertex : graph) {
+    for (auto& vertex : graph_) {
       vertex.second->visited_ = false;
     }
   }
+  bool AddVertices(std::initializer_list<std::string> vertices) {
+    for (auto vertx : vertices) {
+      if (!AddVertex(vertx)) {
+        return false;
+      }
+    }
+    return true;
+  }
 
   bool AddVertex(std::string const& name) {
-    if (graph.find(name) != end(graph)) {
+    if (graph_.find(name) != end(graph_)) {
       return false;
     }
-    graph[name] = nullptr;
-    graph[name] = std::make_shared<Vertex>(graph.find(name)->first);
+    graph_[name] = nullptr;
+    graph_[name] = std::make_shared<Vertex>(graph_.find(name)->first);
     return true;
   }
 
   bool AddEdge(std::string const& from, std::string const& to, int cost = 0,
                int length = 0) {
-    auto from_it = graph.find(from);
-    auto to_it = graph.find(to);
-    if ((from_it == end(graph)) || (to_it == end(graph))) {
+    auto from_it = graph_.find(from);
+    auto to_it = graph_.find(to);
+    if ((from_it == end(graph_)) || (to_it == end(graph_))) {
       return false;
     }
     from_it->second->AddVertex(to_it->second, Edge{cost, length});
+    if (!directed_) {
+      to_it->second->AddVertex(from_it->second, Edge{cost, length});
+    }
     return true;
   }
 
   using Visit = std::function<bool(std::shared_ptr<Vertex>)>;
-
   void BreadthFirstSearch(std::shared_ptr<Vertex> vertex, Visit visit) {
     std::queue<std::shared_ptr<Vertex>> mqueue;
     vertex->visited_ = true;
@@ -79,10 +102,10 @@ class Graph {
       if (!visit(vrtx)) {
         return;
       }
-      for (auto& neighbor : vrtx->adjacency_list_) {
-        if (!neighbor.first->visited_) {
-          mqueue.push(neighbor.first);
-          neighbor.first->visited_ = true;
+      for (auto [neighbor, _] : vrtx->adjacency_list_) {
+        if (!neighbor->visited_) {
+          mqueue.push(neighbor);
+          neighbor->visited_ = true;
         }
       }
     }
@@ -98,6 +121,114 @@ class Graph {
       }
     }
     return visit(vertex);
+  }
+
+  std::vector<std::shared_ptr<Vertex>> BidirectionalSearch(
+      std::shared_ptr<Vertex> start, std::shared_ptr<Vertex> target) {
+    std::vector<std::shared_ptr<Vertex>> path;
+    if ((start == nullptr) || (target == nullptr)) {
+      return path;
+    }
+    std::queue<std::shared_ptr<Vertex>> squeue;
+    squeue.push(start);
+    std::queue<std::shared_ptr<Vertex>> tqueue;
+    tqueue.push(target);
+
+    std::unordered_set<std::string_view> svisited;
+    svisited.insert(start->name_);
+    std::unordered_set<std::string_view> tvisited;
+    tvisited.insert(target->name_);
+
+    std::unordered_map<std::string_view, std::string_view> sparent;
+    sparent[start->name_] = {};
+    std::unordered_map<std::string_view, std::string_view> tparent;
+    tparent[target->name_] = {};
+
+    while (!squeue.empty() && !tqueue.empty()) {
+      BidirectionalSearchBreadthFirst(std::ref(squeue), std::ref(sparent),
+                                      std::ref(svisited));
+      BidirectionalSearchBreadthFirst(std::ref(tqueue), std::ref(tparent),
+                                      std::ref(tvisited));
+      // Is intersected
+      std::string_view intersect{};
+      // std::set_intersection only works on sorted containers
+      for (auto sv : svisited) {
+        if (tvisited.find(sv) != end(tvisited)) {
+          intersect = sv;
+          break;
+        }
+      }
+
+      if (!intersect.empty()) {
+        path.push_back(FindVertedByName(intersect));
+        auto current_vx_name = intersect;
+        while (!sparent[current_vx_name].empty()) {
+          path.push_back(FindVertedByName(sparent[current_vx_name]));
+          current_vx_name = sparent[current_vx_name];
+        }
+        std::reverse(begin(path), end(path));
+        current_vx_name = intersect;
+        while (!tparent[current_vx_name].empty()) {
+          path.push_back(FindVertedByName(tparent[current_vx_name]));
+          current_vx_name = tparent[current_vx_name];
+        }
+        break;
+      }
+    }
+
+    return path;
+  }
+
+ private:
+  void BidirectionalSearchBreadthFirst(
+      std::reference_wrapper<std::queue<std::shared_ptr<Vertex>>> rqueue,
+      std::reference_wrapper<
+          std::unordered_map<std::string_view, std::string_view>>
+          rparent,
+      std::reference_wrapper<std::unordered_set<std::string_view>> rvisited) {
+    auto& visited = rvisited.get();
+    auto& parent = rparent.get();
+    auto& mqueue = rqueue.get();
+    std::shared_ptr<Vertex> parent_vertex{nullptr};
+    if (!mqueue.empty()) {
+      static_cast<void>(PutUnvisitedNeighborsInQueue(
+          rqueue,
+          [&parent_vertex](std::shared_ptr<Vertex> current_vertex) {
+            parent_vertex = current_vertex;
+            return true;
+          },
+          [&visited, &parent, &parent_vertex](std::shared_ptr<Vertex> vertex) {
+            visited.insert(vertex->name_);
+            parent[vertex->name_] = parent_vertex->name_;
+          },
+          [&visited](std::shared_ptr<Vertex> vertex) {
+            return visited.find(vertex->name_) != end(visited);
+          }));
+    }
+  }
+
+  using SetVisited = std::function<void(std::shared_ptr<Vertex>)>;
+  using IsVisited = std::function<bool(std::shared_ptr<Vertex>)>;
+  bool PutUnvisitedNeighborsInQueue(
+      std::reference_wrapper<std::queue<std::shared_ptr<Vertex>>> rqueue,
+      Visit const& visit,
+      SetVisited set_visited =
+          [](std::shared_ptr<Vertex> vrtx) { vrtx->visited_ = true; },
+      IsVisited is_visited =
+          [](std::shared_ptr<Vertex> vrtx) { return vrtx->visited_; }) {
+    auto& mqueue = rqueue.get();
+    auto vertex = mqueue.front();
+    mqueue.pop();
+    if (!visit(vertex)) {
+      return false;
+    }
+    for (auto [neighbor, _] : vertex->adjacency_list_) {
+      if (!is_visited(neighbor)) {
+        set_visited(neighbor);
+        mqueue.push(neighbor);
+      }
+    }
+    return true;
   }
 };
 
--- Graph/tests.cpp
+++ Graph/tests.cpp
@@ -4,8 +4,8 @@
 
 #include "gtest/gtest.h"
 
-#include <iostream>
-#include <memory>
+#include <iostream>  // NOLINT
+#include <memory>    // NOLINT
 
 #include "graph.hpp"
 
@@ -41,16 +41,16 @@ TEST(GraphTests, CreateGraph) {
     return true;
   };
 
-  uut.BreadthFirstSearch(begin(uut.graph)->second, visitor);
+  uut.BreadthFirstSearch(begin(uut.graph_)->second, visitor);
   std::cout << '\n';
   uut.Reset();
 
-  uut.DepthFirstSearch(begin(uut.graph)->second, visitor);
+  uut.DepthFirstSearch(begin(uut.graph_)->second, visitor);
 
   std::cout << '\n';
   uut.Reset();
 
-  uut.DepthFirstSearch(uut.graph["c"],
+  uut.DepthFirstSearch(uut.graph_["c"],
                        [](std::shared_ptr<Graph::Vertex> vertex) {
                          std::cout << vertex->name_ << ' ';
                          if (vertex->name_ == "a") {
@@ -59,4 +59,31 @@ TEST(GraphTests, CreateGraph) {
                          }
                          return true;
                        });
+}
+
+TEST(GraphTest, BidrectionalTest) {
+  Graph uut{false};
+  EXPECT_TRUE(uut.AddVertices({"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                               "10", "11", "12", "13", "14"}));
+  EXPECT_TRUE(uut.AddEdge("0", "4"));
+  EXPECT_TRUE(uut.AddEdge("1", "4"));
+  EXPECT_TRUE(uut.AddEdge("2", "5"));
+  EXPECT_TRUE(uut.AddEdge("3", "5"));
+  EXPECT_TRUE(uut.AddEdge("4", "6"));
+  EXPECT_TRUE(uut.AddEdge("5", "6"));
+  EXPECT_TRUE(uut.AddEdge("6", "7"));
+  EXPECT_TRUE(uut.AddEdge("7", "8"));
+  EXPECT_TRUE(uut.AddEdge("8", "9"));
+  EXPECT_TRUE(uut.AddEdge("8", "10"));
+  EXPECT_TRUE(uut.AddEdge("9", "11"));
+  EXPECT_TRUE(uut.AddEdge("9", "12"));
+  EXPECT_TRUE(uut.AddEdge("10", "13"));
+  EXPECT_TRUE(uut.AddEdge("10", "14"));
+  auto path = uut.BidirectionalSearch(uut.FindVertedByName("0"),
+                                      uut.FindVertedByName("14"));
+
+  for (auto vx : path) {
+    std::cout << vx->name_ << ' ';
+  }
+  std::cout << '\n';
 }

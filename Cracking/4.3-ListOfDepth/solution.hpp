3786df0eb88562eeaed1d6cbebf63c2bab392914
Ahmad Sheikhveisi <ahmad.sheikhveisi@volvocars.com>
2023-09-29 17:33:45 +0200

    Add list of depths and heights

--- Cracking/4.3-ListOfDepth/solution.hpp
+++ Cracking/4.3-ListOfDepth/solution.hpp
@@ -3,11 +3,15 @@
  * Given a binary tree, design an algorithm which creates a linked list of all
  * the nodes at each depth. if you have a tree with depth D, you'll have D
  * linked lists.
+ * 
+ * The depth of a node is the number of edges present in path from the root node of a tree to that node.
+ * The height of a node is the number of edges present in the longest path connecting that node to a leaf node.
  */
 
 #ifndef CRACKING_4_3_LISTOFDEPTH_SOLUTION_HPP_
 #define CRACKING_4_3_LISTOFDEPTH_SOLUTION_HPP_
 
+#include <unordered_map>
 #include <memory>
 #include <vector>
 
@@ -19,8 +23,44 @@ class Solution {
  public:
   using Node = typename BinaryTree<T>::Node;
 
-  LinkedList<std::shared_ptr<Node>> ListOfDepths(BinaryTree<T> const& tree) {
-    return LinkedList<std::shared_ptr<Node>>{tree.root_};
+  std::unordered_map<std::shared_ptr<Node>, size_t> ListOfHeigths(BinaryTree<T> const& tree) {
+    std::unordered_map<std::shared_ptr<Node>, size_t> list{};
+    ListOfHeigthSubTree(tree.root_, list);
+    return list;
+  }
+
+  std::unordered_map<std::shared_ptr<Node>, size_t> ListOfDepths(BinaryTree<T> const& tree) {
+    std::unordered_map<std::shared_ptr<Node>, size_t> list{};
+    ListOfDepthSubTree(tree.root_, 0, list);
+    return list;
+  }
+
+private:
+  size_t ListOfHeigthSubTree(std::shared_ptr<Node> node, std::unordered_map<std::shared_ptr<Node>, size_t>& list) {
+    if (node == nullptr) {
+      // this only happens when only one of the children is
+      // null. so no need to store it.
+      return 0;
+    }
+    if (node->left_ == nullptr && node->right_ == nullptr) {
+      // this is a leaf.
+      list[node] = 0;
+      return 0;
+    }
+    auto depth = std::max(ListOfHeigthSubTree(node->left_, list), ListOfHeigthSubTree(node->right_, list)) + 1;
+    list[node] = depth;
+    return depth;
+  }
+
+  void ListOfDepthSubTree(std::shared_ptr<Node> node, size_t current_height, std::unordered_map<std::shared_ptr<Node>, size_t>& list) {
+    if (node == nullptr) {
+      // this only happens when only one of the children is
+      // null. so no need to store it.
+      return;
+    }
+    list[node] = current_height;
+    ListOfDepthSubTree(node->left_, current_height + 1, list);
+    ListOfDepthSubTree(node->right_, current_height + 1, list);
   }
 };
 
--- Cracking/4.3-ListOfDepth/tests.cpp
+++ Cracking/4.3-ListOfDepth/tests.cpp
@@ -8,7 +8,22 @@
 
 TEST(MinimalHeightBSTTest, CreateBST) {
   Solution<int> uut{};
-  auto temp = uut.ListOfDepths(BinaryTree<int>{0});
-
-  std::cout << temp.front->data->value_ << '\n';
+  BinaryTree<int> tree{5};
+  tree.root_->SetLeft(10);
+  tree.root_->SetRight(15);
+  tree.root_->left_->SetLeft(20);
+  tree.root_->left_->SetRight(25);
+  tree.root_->left_->right_->SetRight(45);
+  tree.root_->right_->SetLeft(30);
+  tree.root_->right_->SetRight(35);
+  auto heights = uut.ListOfHeigths(tree);
+  std::cout << "Heights\n";
+  for (auto const& itm : heights) {
+    std::cout << itm.first->value_ << ' ' << itm.second << '\n';
+  }
+  auto depths = uut.ListOfDepths(tree);
+  std::cout << "Depths\n";
+  for (auto const& itm : depths) {
+    std::cout << itm.first->value_ << ' ' << itm.second << '\n';
+  }
 }

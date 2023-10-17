22a78b654f252174d2caedb81a947c748c3244b2
Ahmad Sheikhveisi <ahmad.sheikhveisi87@gmail.com>
2023-10-17 21:31:54 +0200

    Add brute force solution to path with sum problem

--- Cracking/4.12-PathWithSum/solution.hpp
+++ Cracking/4.12-PathWithSum/solution.hpp
@@ -23,10 +23,37 @@
 #include "binary_search_tree.hpp"
 #include "binary_tree.hpp"
 
-template <typename T>
 class Solution {
  public:
-  using NodePtr = std::shared_ptr<typename BinaryTree<T>::Node>;
+  using NodePtr = std::shared_ptr<BinaryTree<int32_t>::Node>;
+
+  size_t CountPathWithSum(NodePtr node, int32_t sum) {
+    if (node == nullptr) {
+      return 0;
+    }
+    // std::cout << "Start form here " << node->value_ << ' ';
+    auto const start_from_here = CountPathWithSumSubTree(node, sum);
+    // std::cout << ' ' << start_from_here;
+    auto const all_left = CountPathWithSum(node->left_, sum);
+    // std::cout << " All left " << all_left;
+    auto const all_right = CountPathWithSum(node->right_, sum);
+    // std::cout << " All right " << all_right << '\n';
+    size_t res{start_from_here + all_left + all_right};
+    std::cout << node->value_ << ' ' << res << '\n';
+
+    return res;
+  }
+
+  size_t CountPathWithSumSubTree(NodePtr node, int32_t sum) {
+    if (node == nullptr) {
+      return 0;
+    }
+    if (sum == node->value_) {
+      return 1;
+    }
+    return CountPathWithSumSubTree(node->left_, sum - node->value_) +
+           CountPathWithSumSubTree(node->right_, sum - node->value_);
+  }
 };
 
 #endif  // CRACKING_4_12_PATHWITHSUM_SOLUTION_HPP_
--- Cracking/4.12-PathWithSum/tests.cpp
+++ Cracking/4.12-PathWithSum/tests.cpp
@@ -7,5 +7,22 @@
 #include "solution.hpp"
 
 TEST(CheckSubTree, test1) {
-  [[maybe_unused]] Solution<int> uut {};
+  [[maybe_unused]] Solution uut {};
+  BinaryTree<int32_t> bt{0};
+  bt.root_->SetLeft(1);
+  bt.root_->SetRight(2);
+  bt.root_->left_->SetLeft(3);
+  bt.root_->left_->SetRight(4);
+  bt.root_->right_->SetLeft(-3);
+  bt.root_->right_->SetRight(-4);
+  bt.root_->left_->left_->SetLeft(7);
+  bt.root_->left_->left_->SetRight(8);
+  bt.root_->left_->right_->SetLeft(9);
+  bt.root_->left_->right_->SetRight(10);
+  bt.root_->right_->left_->SetLeft(11);
+  bt.root_->right_->left_->SetRight(12);
+  bt.root_->right_->right_->SetLeft(13);
+  bt.root_->right_->right_->SetRight(14);
+
+  std::cout << uut.CountPathWithSum(bt.root_, 10) << '\n';
 }
